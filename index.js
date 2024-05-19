0b93df60bba9017c105929f2b64bb54082188c07
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-05-19 21:10:25 +0600

    added new file and folder

---
+++ index.js
@@ -0,0 +1,9 @@
+function menuBar(){
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.style.display = "flex";
+}
+function crossbar(){
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.style.display = "none";
+
+}
